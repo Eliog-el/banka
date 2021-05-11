aa8062d8f099b3f96984326f22d6aa83680705bc
Eliog Olusola <elijahogunrinade@gmail.com>
2021-05-11 15:30:19 +0100

    UserSignup

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,16 @@
 import express from 'express';
 
+import { v4 as uuidv4 } from 'uuid';
+
 const router = express.Router();
 
-router.get('/users');
+router.post('/signup', (req, res) => {
+    const user = req.body;
+
+    users.push({ ...user, id: uuidv4() });
+
+    res.send(`User with the name ${user.firstName} added to the database!`)
+});
+
+
+export default router;
