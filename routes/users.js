358a03f73a5a21f8d7677a054b112e490132da45
Eliog Olusola <elijahogunrinade@gmail.com>
2021-05-14 12:11:28 +0100

    config eslint and babel to server

--- routes/users.js
+++ routes/users.js
@@ -2,4 +2,19 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/users');
+let users = [];
+
+
+router.get('/', (req, res) => {
+    res.send(users);
+})
+
+router.post('/', (req, res) => {
+    const user = req.body;
+
+    users.push({ ...user, id: uuidv4() });
+
+    res.send(`User with the name ${user.firstName} added to the database!`);
+});
+
+export default router
