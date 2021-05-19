0cd1b01856d0a6d51da2b740e2064b6fd8e1dcee
Eliog Olusola <elijahogunrinade@gmail.com>
2021-05-19 07:09:59 +0100

    created users SignUp endpoint

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,11 +2,13 @@
 import { successResponse, errorResponse } from "../helpers/utilities";
 import { v4 as uuidv4 } from "uuid";
 
-export const login = async (req, res) => {
+export const signUp = async (req, res) => {
   try {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
     const user = req.body;
 
-    const userObj = { ...user, id: uuidv4() };
+    const userObj = { ...user, id: uuidv4(), password: hashedPassword };
 
     users.push(userObj);
 
