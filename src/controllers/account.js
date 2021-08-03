f6b51d76babf06a70b7a64761eb6f25544d3f623
Elijah Ogunrinade <elijah.ogunrinade@theagromall.com.ng>
2021-08-03 12:20:21 +0100

    work on the JWT for both signin and signUp

--- src/controllers/account.js
+++ src/controllers/account.js
@@ -1,21 +1,34 @@
-import { successResponse, errorResponse } from "../helpers/utilities";
 import { v4 as uuidv4 } from "uuid";
-import data from '../data/userData.js';
+import users from '../data/userData.js';
+import { successResponse, errorResponse, getTableContents, wToFile } from '../helpers/utilities';
+
 
 import accounts from "../data/userData"
 
 
 export const createAccount = async (req, res) => {
+  
     try {
-      
+      const { firstName, lastName, password, email } = req.body;
+      const users = await getTableContents('users')
+
+      console.log(users);
+
+      users.find(user => user.email === email)
+      if (!users) { 
+        return errorResponse(res, 404, 'User does not exists') 
+      }
+      if (!users.password) {
+        return errorResponse(res, 403, 'incorrect password')
+      }
       const account = req.body;
-      
       const accountNumber = Math.floor(1000000000 + Math.random() * 9000000000);
       
-      const accountObj = { ...account, id: uuidv4(), accountNumber, firstName, Lastname, email};
+      const accountObj = { ...account, id: uuidv4(), accountNumber, firstName, lastName, email };
 
   
       accounts.push(accountObj);
+      wToFile('accounts', accounts)
   
       return successResponse(res, 200, "data", {
         ...accountObj,
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,114 +1,102 @@
 // import { successResponse, errorResponse } from "../helpers";
-require('dotenv').config();
-import { successResponse, errorResponse, getTableContents, wToFile } from '../helpers/utilities';
-import { v4 as uuidv4 } from 'uuid';
-import bcrypt from 'bcrypt';
-// import cookieParser from 'cookieParser'; 
-import { createTokens, validateToken } from '../helpers/token.js';
-import jwt from 'jsonwebtoken' 
-import users from '../data/userData';
+require("dotenv").config();
+import {
+  successResponse,
+  errorResponse,
+  getTableContents,
+  wToFile,
+} from "../helpers/utilities";
+import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcrypt";
+// import cookieParser from 'cookieParser';
+import { createTokens, validateToken } from "../helpers/token.js";
+import jwt from "jsonwebtoken";
+import users from "../data/userData";
 
 // app.use(cookieParser());
 
 export const signUp = async (req, res) => {
-  
   let type;
-  let isAdmin
+  let isAdmin;
   let tokenObj;
   let user;
 
   if (!isAdmin) {
-    isAdmin = false; type = 'client';
+    isAdmin = false;
+    type = "client";
   } else {
-    isAdmin = isadmin; type = 'staff';
+    isAdmin = isadmin;
+    type = "staff";
   }
- 
+
   try {
     const { firstName, lastName, password, email } = req.validated;
-    const users = await getTableContents('users')
-    // console.log(users)
+    const users = await getTableContents("users");
 
-    users.find(user => user.email === email)
-    if (users) { return errorResponse(res, 409, 'User already exists') }
+    const user = users.find((user) => user.email === email);
+
+    if (user) {
+      return errorResponse(res, 409, "User already exists");
+    }
 
     const salt = await bcrypt.genSalt();
 
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    // const id  = fetchedUser;
-    // if (!isadmin) {
-    // tokenObj = token({ id });
-    // }
-
-     const userObj = { first_name: firstName, last_name: lastName, email: email, id: uuidv4(), password: hashedPassword};
+    const userObj = {
+      first_name: firstName,
+      last_name: lastName,
+      email: email,
+      id: uuidv4(),
+      password: hashedPassword,
+    };
 
     users.push(userObj);
 
-    wToFile('users', users)
-    delete userObj.password
+    wToFile("users", users);
+    // delete userObj.password
 
-    return successResponse(res, 200, 'data', {
+    return successResponse(res, 200, "data", {
       ...userObj,
       token: createTokens(userObj),
       message: `User with the name ${firstName} added to the database!`,
     });
-
   } catch (error) {
     console.log(error);
-    errorResponse(res, 500, 'Internet server error');
+    errorResponse(res, 500, "Internet server error");
   }
 };
 
-// export const signIn = async (req, res) => {
-//   // const users = await getTableContents('users')
-//   const user = users.find((user) => user.email === req.body.email);
-
-//   console.log(user);
-
-//   if (!user) {
-//     return errorResponse(res, 404, 'User not found!');
-//   }
-//   try {
-//     if (await bcrypt.compare(req.body.password, user.password)) {
-//       successResponse(res, 200, 'data',);
-//     } else {
-//       return errorResponse(res, 401, 'Invalid username and password');
-//     }
-//   } catch (err) {
-//     return errorResponse(res, 500, 'An error occurred');
-//   }; 
-// }
-
 export const signIn = async (req, res) => {
+  const { email, password } = req.body;
+  let user;
 
-  const user = users.find((user) => user.email === req.body.email);
+  try {
+    const users = await getTableContents("users");
+    user = users.find((user) => user.email === req.body.email);
+    console.log(user);
 
-  // const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-  // res.json({ accessToken: accessToken })
-
-
-  if (!user) {
-    return errorResponse(res, 404, "User not found!");
-  } else {
-    const accessToken = createTokens(users)
-
-    res.cookie("access-token", accessToken, {
-      maxAge: 60 * 60 * 24 * 30 * 1000,
-    });
-
-    try {
-      if (await bcrypt.compare(req.body.password, user.password)) {
-        successResponse(res, 200, "data", {
-          message: 'Signing In successful',
-        }, { accessToken: accessToken })
-      } else {
-        return errorResponse('User not found')
-      }
-    } catch {
-     return errorResponse(res, 500, 'An error occurred');
+    if (!user) {
+      return errorResponse(res, 404, "User not found!");
     }
-  }
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      successResponse(res, 200, "data");
+    } else {
+      return errorResponse(res, 401, "Invalid username and password");
+    }
 
+    const { id, firstName, lastName, type, isadmin } = user;
+
+    // const tokenObj = { id };
+
+    return successResponse(res, 200, "data", {
+      ...user,
+      token: createTokens(user),
+      message: "User loggin!",
+    });
+  } catch (error) {
+    return errorResponse(res, 500, "SERVER ERROR");
+  }
 };
 
 
@@ -117,11 +105,11 @@ export const getDetails = async (req, res) => {
 
   const foundUser = users.find((user) => user.id === id);
 
-  res.send(foundUser)
-}
+  res.send(foundUser);
+};
 
 export const getAllUser = async (req, res) => {
   var users_response = users;
 
-  return successResponse(res, 200, 'users', users_response);
-}
+  return successResponse(res, 200, "users", users_response);
+};
